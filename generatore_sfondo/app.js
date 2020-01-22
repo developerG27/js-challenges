2ec3008104e2a0be1d66bafed54ebdffa1ab0bc5
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-22 22:48:43 +0100

    Fix

--- generatore_sfondo/app.js
+++ generatore_sfondo/app.js
@@ -18,13 +18,17 @@ var color = [
   "brown"
 ];
 
-document.addEventListener("click", function() {
+document.body.addEventListener("click", function() {
   document.body.style.backgroundColor = color[parseInt(Math.random * 10)];
 });
-
 
 /**
  * TODO: color è un array in questo caso, e quindi se vuoi inserire "color" come sfondo devi metter color[0] (un numero a scelta, tra 0 e 7)
  * TODO: siccome però deve essere uno sfondo casuale devi inserire color[parseInt(Math.random * 10)]
  * TODO: mi rispieghi questi due passaggi
+ */
+
+/**
+ * TODO: l'allacciamento all'evento si fa al document quindi non bisogna scrivere document.body ma solo document.addEventListener (per quello ti ho detto di fare di nuovo il pull)
+ * TODO: Math.random è una funzione e quindi si scrive così Math.random() e non Math.random
  */
