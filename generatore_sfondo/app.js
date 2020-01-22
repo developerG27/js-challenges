ba49232c4870fbffca78ada49e3f03b593120d1e
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-22 22:34:17 +0100

    todo ultimi due step

--- generatore_sfondo/app.js
+++ generatore_sfondo/app.js
@@ -19,19 +19,12 @@ var color = [
 ];
 
 document.addEventListener("click", function() {
-  document.querySelector("body").style.backgroundColor = color;
+  document.body.style.backgroundColor = color[parseInt(Math.random * 10)];
 });
 
-/* TODO: ho dichiarato la variabile
--inserito i colori nell'array
--aggiunto l'evento click e la funzione
--ho richiamato il body
--gli ho dato lo stile al background
--dove sto sbagliando? */
 
 /**
- * TODO: Prima di tutto se vuoi andare a capo con i TODO devi fare così e non come hai fatto sopra.
  * TODO: color è un array in questo caso, e quindi se vuoi inserire "color" come sfondo devi metter color[0] (un numero a scelta, tra 0 e 7)
  * TODO: siccome però deve essere uno sfondo casuale devi inserire color[parseInt(Math.random * 10)]
- * TODO: Cancella i tuoi TODO una volta che ricevi i miei suggerimenti, quando hai risolto e hai ancora dubbi, scrivene di nuovi.
+ * TODO: mi rispieghi questi due passaggi
  */
