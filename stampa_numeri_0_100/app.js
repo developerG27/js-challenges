6095a13aca946ae0fd2a8b8acfbd147e99d5ce95
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-22 23:34:45 +0100

    todo stampa numeri

--- stampa_numeri_0_100/app.js
+++ stampa_numeri_0_100/app.js
@@ -3,3 +3,11 @@
  * - Crei un ciclo for che fa un innerHTML +=
  * - Ricordati di aggiungere un <br>
  */
+
+var number = []; 
+
+for(var i = 0; i <number.length; i++){
+  document.body.innerHTML += number[i] + '<br>';
+}
+
+//TODO: .length serve? è giusto dichiarare la variabile cosi?
