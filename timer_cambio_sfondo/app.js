093b30c91e79794204ebafe6e06ec6153f165a39
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-22 22:57:52 +0100

    aggiunto timer_cambio_sfondo

---
+++ timer_cambio_sfondo/app.js
@@ -0,0 +1,7 @@
+/**
+ * Cambia lo sfondo automaticamente del tuo browser ogni 3 secondi
+ * - Creare un array con diversi colori
+ * - Salvare il documento in una variabile
+ * - creare un setInterval che ogni 3 secondi cambi lo sfondo
+ * - Il colore dello sfondo viene scelto in maniera random sempre in base all'array
+ */
