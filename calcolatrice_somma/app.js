924f241fcc4daee34bf633abbf6e07530d3bc8da
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-23 00:17:50 +0100

    terminato calcolatrice

--- calcolatrice_somma/app.js
+++ calcolatrice_somma/app.js
@@ -3,3 +3,8 @@
  * - Crei una funzione chiamata somma che riceve due parametri
  * - Ritorni numero1 + numero2
  */
+
+function somma(numb1, numb2){
+  document.write (numb1 + numb2)
+}
+somma(18, 96);
