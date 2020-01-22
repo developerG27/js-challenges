d8fd2e909962d5b096f03293daf96d070d3eb016
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-22 22:59:31 +0100

    es stampa nomi terminato

--- stampa_nomi/app.js
+++ stampa_nomi/app.js
@@ -3,3 +3,8 @@
  * - Crei due variabili
  * - e al documento fai un innerHTML += del tuo nome e cognome
  */
+
+var name = 'Sabrina';
+var surname = 'Paola';
+
+document.body.innerHTML += name + '<br>' + surname;
