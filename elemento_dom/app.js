a55d96ffd8dd81660801ab091d3286f04d872fb2
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-23 15:49:33 +0100

    completato

--- elemento_dom/app.js
+++ elemento_dom/app.js
@@ -3,3 +3,7 @@
  * - https://www.w3schools.com/js/js_htmldom_nodes.asp
  * - Il paragrafo deve contenere la scritta Hello world
  */
+
+var paragrafo = document.createElement("p");
+paragrafo.innerHTML = "<p>HELLO WORLD</p>";
+document.body.appendChild(paragrafo);
