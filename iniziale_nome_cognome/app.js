315efa6574496e902b936f0a56e33498ba82140e
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-23 09:37:32 +0100

    aggiunto iniziale_nome_cognome

---
+++ iniziale_nome_cognome/app.js
@@ -0,0 +1,6 @@
+/**
+ * Stampa sul documento le iniziali del tuo nome e cognome
+ * - Salva in due variabili il tuo nome e cognome
+ * - Usa charAt per procurarti le iniziali del tuo nome e cognome
+ * - Stampa il risultato sul documento
+ */
