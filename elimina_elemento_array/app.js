0ea56d13d6cb5c8c75dbc90fd022299a78f48a6d
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-23 09:35:15 +0100

    aggiunto elimina_elemento_array

---
+++ elimina_elemento_array/app.js
@@ -0,0 +1,6 @@
+/**
+ * A partire da un array di 6 elementi, elimina il terzo elemento
+ * - Crea un array di 6 elementi
+ * - Elimina il terzo elemento
+ * - Stampa sul documento il nuovo array
+ */
