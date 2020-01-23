527f0d4dc5d67af9d37cf6c54f6333546ad1932a
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-23 09:40:30 +0100

    aggiunto elemento_dom

---
+++ aggiungi_elemento_dom/app.js
@@ -0,0 +1,5 @@
+/**
+ * Aggiunge un paragrafo sul documento usando solo javascript
+ * - https://www.w3schools.com/js/js_htmldom_nodes.asp
+ * - Il paragrafo deve contenere la scritta Hello world
+ */
