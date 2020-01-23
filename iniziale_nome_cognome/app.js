7d4f0edfc21cb31441265a21fd57b063934ec806
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-23 15:35:34 +0100

    completato
    git commit -m completato

--- iniziale_nome_cognome/app.js
+++ iniziale_nome_cognome/app.js
@@ -4,3 +4,8 @@
  * - Usa charAt per procurarti le iniziali del tuo nome e cognome
  * - Stampa il risultato sul documento
  */
+
+var name = 'Sabrina';
+var surname = 'Paola';
+
+document.body.innerHTML = name.charAt(0) + '<br>' + surname.charAt(0);
