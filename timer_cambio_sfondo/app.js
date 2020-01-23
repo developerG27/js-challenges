b7a3123424f33933fc30398f4275dd8d91a52bb3
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-23 17:12:33 +0100

    fix

--- timer_cambio_sfondo/app.js
+++ timer_cambio_sfondo/app.js
@@ -5,16 +5,17 @@
  * - creare un setInterval che ogni 3 secondi cambi lo sfondo
  * - Il colore dello sfondo viene scelto in maniera random sempre in base all'array
  */
-var color = [
-  "Red",
-  "Orange",
-  "Yellow",
-  "Brown",
-  "Blue",
-];
+var color = ["Red", "Orange", "Yellow", "Brown", "Blue"];
 
-setInterval(function(){
-  document.body.style.backgroundColor = color[parseInt(Math.random() *10)]
-}, 3000)
+document.body.style.backgroundColor = color[parseInt(Math.random() * 10)];
+
+setInterval(function() {
+  document.body.style.backgroundColor = color[parseInt(Math.random() * 10)];
+}, 3000);
 
 //TODO: perchè parte dal bianco e non direttamente dal rosso?
+
+/**
+ * TODO: Perché di default lo sfondo è bianco e dopo tre secondi gli metti un colore random, per risolvere potresti fare cosi:
+ *
+ * */
