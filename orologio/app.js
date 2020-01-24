7daf3580fe5dfcaf4ab7f6aef659a7e42f00cd57
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-24 12:06:18 +0100

    todo

--- orologio/app.js
+++ orologio/app.js
@@ -2,3 +2,13 @@
  * Crea sul document un orologio funzionante
  * https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
  */
+
+function time() {
+  var clock = new Date();
+  var hour = clock.getHours();
+  var minute = clock.getMinutes();
+  var second = clock.getSeconds();
+  document.body.innerHTML = hour + ":" + minute + ":" + second;
+}
+
+//TODO: fino a qui ho capito, poi non riesco a capire l'esempio, mi spieghi?
