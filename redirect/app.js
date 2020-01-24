a64a2e04fd10be9640e373dde6da555d91d04dc6
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-24 11:42:10 +0100

    correzioni

--- redirect/app.js
+++ redirect/app.js
@@ -6,3 +6,8 @@
 setInterval(function() {
   window.location.replace("http://www.google.com");
 }, 3000);
+
+/**
+ * TODO: Funziona ma non è il modo giusto per farlo, in quanto setInterval è "una funzione che viene ripetuta ad ogni intervallo di tempo, in questo caso 3000 millisecondi"
+ * TODO: La funziona giusta è setTimeOut in quanto "esegue il codice una volta dopo i 3000 millisecondi"
+ */
