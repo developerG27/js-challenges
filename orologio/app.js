2bfe09ac14938ee0e5bc8c82be6af5f912a08f33
Gian Pumayalla <g.pumayalla@hotmail.com>
2020-01-24 14:55:31 +0100

    fix

--- orologio/app.js
+++ orologio/app.js
@@ -9,10 +9,25 @@ function clock() {
   var minute = time.getMinutes();
   var second = time.getSeconds();
 
-  setInterval(function clock(){
+  document.body.innerHTML = hour + ":" + minute + ":" + second;
+
+  /*setInterval(function clock() {
     document.body.innerHTML = hour + ":" + minute + ":" + second;
-  }, 1000);
+  }, 1000);*/
 }
 
+/*
+parametro1: una funzione anonima
+parametro2: i millesecondi
+
+setInterval(parametro1
+    Qui si richiama la tua funzione clock()
+  , parametro2)
+*/
 
 //TODO: non mi appare niente, dove sto sbagliando? Ho aggiunto setInterval e i millesecondi come mi hai scritto nel todo precedente
+
+/**
+ * TODO: Quando crei una funzione poi la devi richiamare, è come se avessi insegnato al pc una nuova cosa, ma non gli dici come farla
+ * TODO: Il setInterval va fuori della funzione, perchè te vuoi richiamare quella funzione ogni 1000 millesecondi
+ */
