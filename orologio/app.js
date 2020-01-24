d58cfa1cafd5e6eac989dba7d00342ed3bca1359
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-24 12:21:02 +0100

    todo

--- orologio/app.js
+++ orologio/app.js
@@ -3,12 +3,12 @@
  * https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
  */
 
-function time() {
-  var clock = new Date();
-  var hour = clock.getHours();
-  var minute = clock.getMinutes();
-  var second = clock.getSeconds();
-  document.body.innerHTML = hour + ":" + minute + ":" + second;
+function clock() {
+  var time = new Date();
+  var hour = time.getHours();
+  var minute = time.getMinutes();
+  var second = time.getSeconds();
+  document.body.innerHTML = hour + ":" + mimute + ":" + second;
 }
 
-//TODO: fino a qui ho capito, poi non riesco a capire l'esempio, mi spieghi?
+//TODO: mi riscrivi la spiegazione?
