b15e9e15a1e5903f414a59f9f7cc9dff88ceb863
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-24 11:36:41 +0100

    terminato

--- redirect/app.js
+++ redirect/app.js
@@ -2,3 +2,7 @@
  * Crea uno script che dopo tre secondi dal caricamento dell'HTML fa un redirect a http:google.com
  * -https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
  */
+
+setInterval(function() {
+  window.location.replace("http://www.google.com");
+}, 3000);
