185d8dd5c4ac0b7da8760c7e2b8b3697ddfcec34
Sabrina Paola <sabrinapaola180896@gmail.com>
2020-01-28 11:35:25 +0100

    cards con img e title

--- level-2/chiamata-asincrona/app.js
+++ level-2/chiamata-asincrona/app.js
@@ -6,7 +6,19 @@ blog.open("get","https://jsonplaceholder.typicode.com/photos",true, null, null);
 blog.send(null); //solo con get è null
 
 blog.onreadystatechange = function(){ //Attende il cambio dello stato poi fa partire una funzione
-  if(blog.readyState === 4)
-}
-
-//TODO: fino a qui ci sono, poi come procedo? (la guida arriva fino agli status/sicurezza)
+  if(blog.readyState === 4){
+    var response = JSON.parse(blog.response);
+    var graphic = document.getElementById('cards')
+    for(var i = 0; i < 50; i++){
+      graphic.innerHTML +=
+      `<div class="col">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <img class="card-img-top" src="${response[i].url}">
+            <h5 class="card-title">${response[i].title}</h5>
+          </div>
+        </div>
+      </div> `
+    }
+  }
+};
